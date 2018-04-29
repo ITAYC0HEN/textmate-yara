dee43484d6054061df8b6f86e99257f32cca3793
Thomas Gardner <tpgardner19@gmail.com>
2018-04-29 15:37:05 -0600

    rearranged some lines

--- yara/src/extension.ts
+++ yara/src/extension.ts
@@ -14,14 +14,14 @@ export function activate(context: vscode.ExtensionContext) {
     let referenceDisposable: vscode.Disposable = vscode.languages.registerReferenceProvider(YARA, new YaraReferenceProvider());
     let completionDisposable: vscode.Disposable = vscode.languages.registerCompletionItemProvider(YARA, new YaraCompletionItemProvider(), '.');
     let diagnosticCollection: vscode.DiagnosticCollection = vscode.languages.createDiagnosticCollection('yara');
-    let saveSubscription: vscode.Disposable = vscode.workspace.onDidSaveTextDocument(function () { CompileRule(null, diagnosticCollection); });
     let compileCommand: vscode.Disposable = vscode.commands.registerCommand("yara.CompileRule", function () { CompileRule(null, diagnosticCollection); });
+    let saveSubscription: vscode.Disposable = vscode.workspace.onDidSaveTextDocument(function () { CompileRule(null, diagnosticCollection); });
     context.subscriptions.push(definitionDisposable);
     context.subscriptions.push(referenceDisposable);
     context.subscriptions.push(completionDisposable);
     context.subscriptions.push(diagnosticCollection);
-    context.subscriptions.push(saveSubscription);
     context.subscriptions.push(compileCommand);
+    context.subscriptions.push(saveSubscription);
 };
 
 export function deactivate(context: vscode.ExtensionContext) {
